bbc69b4c4d7b6d4206c17d130151cf90900479a7
killerbhai <58168649+killerbhai@users.noreply.github.com>
2021-12-01 18:34:20 -0800

    Update 🔧 Exchange.js

--- commands/🔧 Exchange.js
+++ commands/🔧 Exchange.js
@@ -13,9 +13,9 @@ var dgb = Libs.ResourcesLib.userRes("dgb")
 
 Bot.sendKeyboard(
   "🆔 Menu",
-  "In this section, you can exchange DigiByte for Stars.\nAll Stars you will receive will be added to your account.\n\n🔻 *Game currency rate:*\n▪️ 1 DigiByte = 10 🌟\n\n🔻 *Your DigiByte:*\n" +
+  "In this section, you can exchange LitCoin for Stars.\nAll Stars you will receive will be added to your account.\n\n🔻 *Game currency rate:*\n▪️ 0.0024 LitCoin = 10 🌟\n\n🔻 *Your LitCoin:*\n" +
     dgb.value().toFixed(8) +
-    " DGB\n\n👇 Enter DigiByte amount you want to exchange:"
+    " LTC\n\n👇 Enter LitCoin amount you want to exchange:"
 )
 Bot.runCommand("onExc")
 
