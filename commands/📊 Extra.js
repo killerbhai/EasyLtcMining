066501562dc1ab5ae8239871a7c7ce2c6d75fd73
killerbhai <58168649+killerbhai@users.noreply.github.com>
2021-12-01 18:32:09 -0800

    Update 📊 Extra.js

--- commands/📊 Extra.js
+++ commands/📊 Extra.js
@@ -17,6 +17,6 @@ var wds = Libs.ResourcesLib.anotherUserRes("wds", 718477)
 var usr = Libs.ResourcesLib.anotherUserRes("usr", 718477)
 var dgus = mrd*wds.value()
 
-Bot.sendKeyboard("🏧 Payments, 💼 Set DGB Wallet,\n🆔 Menu", "📊 *Statistics*\n\nTotal Players: *"+usr.value().toFixed(0)+"*")
-Bot.sendInlineKeyboard([{title:"Total Payment Receipt",url:"t.me/DigiByteMiningPay"}], "Total payed out:\n*"+wds.value().toFixed(6)+" DGB ~ "+dgus.toFixed(2)+"$*")
+Bot.sendKeyboard("🏧 Payments, 💼 Set LTC Wallet,\n🆔 Menu", "📊 *Statistics*\n\nTotal Players: *"+usr.value().toFixed(0)+"*")
+Bot.sendInlineKeyboard([{title:"Total Payment Receipt",url:"t.me/DigiByteMiningPay"}], "Total payed out:\n*"+wds.value().toFixed(6)+" LTC ~ "+dgus.toFixed(2)+"$*")
 Bot.sendInlineKeyboard([{title:"🔴 Total Deposit Receipt",url:"t.me/DigiByteMiningPay"}], "🔻 New channel - Payment list 🔻")
